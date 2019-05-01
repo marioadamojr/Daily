338363e7bf89633f4bf03bfbafc6ac14d399ba1f
Mario Adamo Jr <marioadamojr@gmail.com>
2019-05-01 01:35:14 +0000

    Six

--- lib/daily/cli.rb
+++ lib/daily/cli.rb
@@ -17,15 +17,19 @@ class Daily::CLI
   end
 
   def menu
-    puts "Enter the number of the deal you'd like more info on or type exit:"
     input = nil
     while input != "exit"
+      puts "Enter the number of the deal you'd like more info on or type list to see the deals again or type exit:"
       input = gets.strip.downcase
       case input
       when "1"
         puts "More info on Deal 1..."
       when "2"
         puts "More info on Deal 2..."
+      when "list"
+        list_deals
+      else
+        puts "Please select a valid option, 1, 2, list, or exit"
       end
     end
   end
